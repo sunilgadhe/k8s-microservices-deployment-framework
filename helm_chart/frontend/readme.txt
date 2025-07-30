5f69a66f0f3034360bd96fc1140c0292d729f1d5
sunilgadhe <sunilgade.cloud@gmail.com>
2025-07-30 15:56:19 +0100

    up

--- helm_chart/frontend/readme.txt
+++
@@ -1,8 +0,0 @@
-************************************************
-Check aws-load-balancer-controller logs for ssl certificate related errors
-************************************************
-1. To get all pods
-kubectl get pods --all-namespaces 
-
-2. Check logs
-kubectl logs aws-load-balancer-controller-5c648b46b5-frpxd -n kube-system
--- infra/eksctl/eks_packages/installation_script.sh
+++ infra/eksctl/eks_packages/installation_script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-eksctl utils associate-iam-oidc-provider --cluster my-eks --approve
+eksctl utils associate-iam-oidc-provider --cluster my-eks-cluster --approve
 
 eksctl create iamserviceaccount \
   --name ebs-csi-controller-sa \
   --namespace kube-system \
-  --cluster my-eks \
+  --cluster my-eks-cluster \
   --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy \
   --approve \
   --role-only \
   --role-name AmazonEKS_EBS_CSI_Driver_Role \
   --region us-east-1
 
-eksctl create addon --name aws-ebs-csi-driver --cluster my-eks --service-account-role-arn arn:aws:iam::910322382104:role/AmazonEKS_EBS_CSI_Driver_Role --region us-east-1 --force
+eksctl create addon --name aws-ebs-csi-driver --cluster my-eks-cluster --service-account-role-arn arn:aws:iam::910322382104:role/AmazonEKS_EBS_CSI_Driver_Role --region us-east-1 --force
 
 #Install Nginx Ingress Controller
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
